a334eb1c2df95248165e6beb2dd2d1a49081bf82
Andrey <saponov.andrey3@gmail.com>
2023-02-13 20:20:04 +0200

    v 2

--- my_app.py
+++ my_app.py
@@ -6,8 +6,7 @@ from PyQt5.QtWidgets import (
         QGroupBox, QRadioButton,
         QPushButton, QLabel, QListWidget, QLineEdit)
 
-from instr import *
-from second_win import *
+
 
        
 class MainWin(QWidget):
