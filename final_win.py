62e52fe815f9bfaadaf9c5b53d1eb2fa630a8271
Andrey <125194252+AndreySaponov@users.noreply.github.com>
2023-03-03 18:44:16 +0200

    v 2.2
    dell code

--- final_win.py
+++ final_win.py
@@ -1,46 +1,4 @@
-from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
-from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # перевірка типів значень, що вводяться
-from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, QGridLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
-        
-from instr import *
 
-class FinalWin(QWidget):
-    def __init__(self, exp):
-        ''' вікно, в якому проводиться опитування '''
-        super().__init__()
-
-        #отримуємо дані про експеримент
-        self.exp = exp
-
-         # створюємо та налаштовуємо графічні елементи:
-        self.initUI()
-
-         #Встановлює зв'язки між елементами
-         # self.connects()
-
-         #Встановлює, як виглядатиме вікно (напис, розмір, місце)
-        self.set_appear()
-        
-         # старт:
-        self.show()
-    def results(self):
-        if self.exp.person.age < 7:
-            self.index = 0
-            return "нет данных для такого возраста"
-        self.index = (4 * (int(self.exp.test1) + int(self.exp.test2) + int(self.exp.test3)) - 200) / 10
-        if self.exp.person.age == 7 or self.exp.person.age == 8:
-            if self.index >= 21:
-                return txt_res1
-            elif self.index < 21 and self.index >= 17:
-                return txt_res2
-            elif self.index < 17 and self.index >= 12:
-                return txt_res3
-            elif self.index < 12 and self.index >= 6.5:
-                return txt_res4
             else:
                 return txt_res5
         if self.exp.person.age == 9 or self.exp.person.age == 10:
