842f2fb49182588b97ada47de163c9c8cebc5b6a
Andrey <125194252+AndreySaponov@users.noreply.github.com>
2023-06-28 19:33:49 +0300

    Update my_app.py
    v3

--- my_app.py
+++ my_app.py
@@ -1,10 +1,4 @@
-from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
-from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # перевірка типів значень, що вводяться
-from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, QGridLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
+
 
 
 
